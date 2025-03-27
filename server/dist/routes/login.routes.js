cd2d4e51b8458bd843382299117348d751507403
Uppula Saikumar <107742169+UppulaSaikumar@users.noreply.github.com>
2025-03-27 12:51:59 +0530

    Delete server directory

--- server/dist/routes/login.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = loginRoutes;
-const auth_controllers_1 = require("../controllers/auth.controllers");
-function loginRoutes(app) {
-    app.post("/api/login", auth_controllers_1.login);
-    app.post("/api/register", auth_controllers_1.register);
-}
--- server/routes/login.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from "express";
-import { login, register } from "../controllers/auth.controllers";
-
-export default function loginRoutes(app: Application): void {
-    app.post("/api/login", login);
-    app.post("/api/register", register);
-}
