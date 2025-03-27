ae5cc54d2cd064224cde35546edd7fe0b5296fbb
Uppula Saikumar <107742169+UppulaSaikumar@users.noreply.github.com>
2025-03-27 12:48:16 +0530

    Delete client directory

--- client/src/global-config.ts
+++
@@ -1,6 +0,0 @@
-const API_URL = `http://localhost:3000` ; //process.env.REACT_APP_API_URL;
-// console.log("uuuddd",import.meta.env.REACT_APP_API_URL);
-// console.log("dddduuuu",process.env.REACT_APP_API_URL);
-
-
-export { API_URL }
